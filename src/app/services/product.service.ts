607e465c18a06d1ec61382f2653334b84ee93bec
DlopezS98 <01dlopezs98@gmail.com>
2020-10-20 10:32:37 -0600

    Integration / setup firebase in the project

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Product } from '../models/Product';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  productsCollection;
+  products: Observable<Product[]>;
+  ProductDoc;
 
-  constructor() { }
+  constructor(public db: AngularFirestore) {
+    this.products = this.db.collection('Products').valueChanges();
+  }
+
+  GetProducts(){
+    return this.products;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB16_3M3fBtMVW2U4ovTEUOIAMsjIdqn0g",
+    authDomain: "angular-firestore-products.firebaseapp.com",
+    databaseURL: "https://angular-firestore-products.firebaseio.com",
+    projectId: "angular-firestore-products",
+    storageBucket: "angular-firestore-products.appspot.com",
+    messagingSenderId: "294888903507",
+    appId: "1:294888903507:web:5d49bcc908d85f174707a6",
+    measurementId: "G-MW1MCNBFTR"
+  }
 };
 
 /*
